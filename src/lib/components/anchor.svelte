d3f6c03690b858cd973153316e5c87655b6fee67
oreqizer <oreqizer@gmail.com>
2022-06-12 12:36:06 +0200

    feat: work, articles, life, social

--- src/lib/components/anchor.svelte
+++ src/lib/components/anchor.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
   export let href: string;
+  export let rel: string = "noopener noreferrer";
 </script>
 
-<a {href} class="underline text-link dark:text-glitch-blue"><slot /></a>
+<a {href} {rel} class="underline text-link dark:text-glitch-blue"><slot /></a>
 
 <style>
   a::after {
