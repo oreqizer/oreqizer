07c493f8ffa1e12b5b942aca1b90e988fdf2724c
oreqizer <oreqizer@gmail.com>
2024-01-25 19:11:58 +0100

    next SEO things

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,24 @@ import remarkGfm from 'remark-gfm';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  async redirects() {
+    return [
+      {
+        source: '/web-app-rendering-101',
+        destination:
+          'https://medium.com/code-kiwi-com/web-app-rendering-101-6ad2e01b0bb4',
+        basePath: false,
+        permanent: false,
+      },
+      {
+        source: '/the-math-of-fitness',
+        destination:
+          'https://medium.com/@oreqizer/the-math-of-fitness-dbcf11971617',
+        basePath: false,
+        permanent: false,
+      },
+    ];
+  },
 };
 
 const withMDX = createMDX({
