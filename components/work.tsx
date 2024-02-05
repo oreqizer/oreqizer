c05a666823f475d4adff04fbefa4a1646a3cf191
oreqizer <oreqizer@gmail.com>
2024-02-05 21:00:37 +0100

    expand work positions

--- components/work.tsx
+++ components/work.tsx
@@ -5,19 +5,19 @@ import type { StaticImageData } from 'next/image';
 export default function Work({
   title,
   description,
-  position,
-  timeline,
   link,
   src,
-  responsibilities,
+  positions,
 }: {
   title: React.ReactNode;
   description: React.ReactNode;
-  position: React.ReactNode;
-  timeline: React.ReactNode;
   link: string;
   src: StaticImageData;
-  responsibilities?: React.ReactNode[];
+  positions: {
+    title: string;
+    timeline: string;
+    responsibilities: string[];
+  }[];
 }): React.JSX.Element {
   return (
     <div className="my-4 space-y-4 border-2 border-black p-4 dark:border-white">
@@ -35,21 +35,25 @@ export default function Work({
         />
       </div>
 
-      <div className="space-y-2">
+      <div className="space-y-4">
         <div>{description}</div>
 
-        <div className="font-bold">{position}</div>
+        {positions.map(({ title, timeline, responsibilities }) => (
+          <div className="space-y-2" key={title}>
+            <div className="font-bold">{title}</div>
 
-        <div className="text-sm">{timeline}</div>
+            <div className="text-sm">{timeline}</div>
 
-        {responsibilities !== undefined && responsibilities.length > 0 && (
-          <ul className="md:ml-4">
-            {responsibilities.map((item, i) => (
-              // eslint-disable-next-line react/no-array-index-key -- static list
-              <li key={i}>{item}</li>
-            ))}
-          </ul>
-        )}
+            {responsibilities.length > 0 && (
+              <ul className="md:ml-4">
+                {responsibilities.map((item, i) => (
+                  // eslint-disable-next-line react/no-array-index-key -- static list
+                  <li key={i}>{item}</li>
+                ))}
+              </ul>
+            )}
+          </div>
+        ))}
       </div>
     </div>
   );
