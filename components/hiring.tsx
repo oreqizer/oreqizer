b778dbfdeb41da20d30165ff7d8b811c5b127f30
oreqizer <oreqizer@gmail.com>
2024-02-06 20:50:41 +0100

    add tagline

--- components/hiring.tsx
+++ components/hiring.tsx
@@ -2,17 +2,24 @@ import * as React from 'react';
 
 export default function Hiring(): React.JSX.Element {
   return (
-    <div className="my-12 whitespace-nowrap text-center text-2xl font-bold sm:text-5xl">
-      <span className="text-glitch-red">&gt;</span>
-      <span className="text-glitch-blue">&gt; </span>
-      <a
-        className="text-white"
-        href="mailto:boris@oreqizer.com?subject=Hey%20Boris,%20come%20work%20for%20us!%20💰"
-      >
-        <span>OPEN FOR HIRE</span>
-      </a>
-      <span className="text-glitch-blue"> &lt;</span>
-      <span className="text-glitch-red">&lt;</span>
+    <div className="my-12 space-y-6 text-center">
+      <div className="my-12 whitespace-nowrap text-center text-2xl font-bold sm:text-5xl">
+        <span className="text-glitch-red">&gt;</span>
+        <span className="text-glitch-blue">&gt; </span>
+        <a
+          className="text-white"
+          href="mailto:boris@oreqizer.com?subject=Hey%20Boris,%20come%20work%20for%20us!%20💰"
+        >
+          <span>OPEN FOR HIRE</span>
+        </a>
+        <span className="text-glitch-blue"> &lt;</span>
+        <span className="text-glitch-red">&lt;</span>
+      </div>
+
+      <div className="text-xl sm:text-2xl">
+        Hiring me you get the <strong>power of four engineers</strong> for the{' '}
+        <strong>price of two</strong>!
+      </div>
     </div>
   );
 }
